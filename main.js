b9a3df6f8267fe5accf6cdb7f24583995d6c8879
alanchiem <chiemalan@gmail.com>
2022-07-18 12:47:01 -0700

    fixed margins when scrolling, removed home

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ function myFunction(){
     if (window.pageYOffset >= sticky) {
         navbar.classList.add("sticky");
     } else {
-        navbar.classList.remove("sticky");
+        
     }
 
 }
