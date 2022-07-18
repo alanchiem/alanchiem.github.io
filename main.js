8d121ed71be34c01f360ee9ff3c2496db486be5f
alanchiem <chiemalan@gmail.com>
2022-07-17 21:30:39 -0700

    added grid with masonry

--- main.js
+++ main.js
@@ -5,3 +5,10 @@ toggle.addEventListener('click', function(){
     collapse.forEach(col=>col.classList.toggle("collapse-toggle")
     )
 })
+
+// with masonry
+
+new Masonry("#posts .grid",{
+    itemSelector :'.grid-item',
+    gutter: 20
+});
